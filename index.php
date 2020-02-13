e32331d099bc0faa50f1b662b23d1a8383b00cb3
eduard <eduard.bindila@gmail.com>
2020-02-13 22:14:32 +0200

    test again

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once('config/helpers.php');
 require_once($_PATH['COMMON_BACKEND'].'functions.php');
 
-echo 'ET calls home';
+echo 'ET calls home yep';
 
 
 include($_PATH['COMMON_LAYOUT'].'head.php');
