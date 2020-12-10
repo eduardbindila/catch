fe1624eb5d97be49e773f31da728d0196eef8f25
eduardbindila <eduard.bindila@gmail.com>
2020-12-10 01:30:12 +0100

    Update send_quote.php

--- modules/projects/controllers/send_quote.php
+++ modules/projects/controllers/send_quote.php
@@ -106,7 +106,7 @@ $conn = $QueryBuilder->dbConnection();
 				$message .= "--{$mime_boundary}--"; 
 			} else {
 
-				$headers = "MIME-Version: 1.0" . "\r\n";
+				$headers .= "MIME-Version: 1.0" . "\r\n";
 				$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 				$message = $htmlContent;
