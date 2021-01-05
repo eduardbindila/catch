06f088b1f2f3d8f6a8451a69ee0fd2e859842282
eduardbindila <eduard.bindila@gmail.com>
2021-01-06 00:12:29 +0100

    added products_page

---
+++ modules/admin/views/products.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function() {
+    var projectsTable = $('.projects_table').DataTable({
+        "ajax": {
+            "url": "/ajax/getProducts/",
+            "dataSrc": ""
+        },
+    
+        pageLength: 100,
+            "paging":   true,
+            "ordering": false,
+            "searching": true,
+        rowId: 'category_slug',
+          
+        responsive: true,
+        order: [1],
+        "columns": [
+             { 
+                "data": "product_image",
+                className: "product_image",
+                "render" : function(data, type, row) {
+                     if(!data){
+                        return '<img src="http://ideyafoana.com/api/public/storage/photo/no-image.png" class="table-image" />'
+                        
+                    } else {
+                          return '<img src="'+data+'" class="table-image" />'
+                    }                       
+                  } 
+
+            },
+            { 
+                "data": "id",
+                "render" : function(data, type, row) {
+                    return '<a href="../cart/product/'+data+'" target="_blank">'+data+'</a>'
+                  } 
+            },
+            { 
+                "data": "product_name"
+            }
+            ,
+            { 
+                "data": "manufacturer"
+            } ,
+            { 
+                "data": "is_temporary"
+            },
+            { 
+                "data": "initial_price"
+            }
+        ],
+        "initComplete": function(settings, json) {
+        }
+
+    });
+
+});
--- modules/admin/views/products_view.php
+++ modules/admin/views/products_view.php
@@ -1,25 +1,17 @@
-
-<script type="text/javascript">
-    var isDisabled = 0;
-    var insertResult = <?php echo $insertResult?>;
-
-</script>
-
-
 <section class="content projectPage">
     <div class="container-fluid">
         <div class="block-header">
-            <h2>Designers</h2>
+            <h2>Products</h2>
         </div>
         <div class="row">
            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="card">
                     <div class="header">
-                        <h2>Designers Details</h2>
+                        <h2>Products Details</h2>
                         <ul class="header-dropdown m-r-0">
                             <li>
                                 <button class="btn btn-success waves-effect" data-toggle="modal"data-target="#addNew-modal">
-                                    Add Designer
+                                    Add Products
                                 </button>
                             </li>
                         </ul>
@@ -29,18 +21,21 @@
                             <div class="col-lg-12">
                                 <div class="alert addUserError hidden bg-pink alert-dismissible" role="alert">
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-                                    Designer could not be added. <b>Please contact the administrator</b>
+                                    Product could not be added. <b>Please contact the administrator</b>
                                 </div>
                                 <div class="alert addUserSuccess hidden alert-success alert-dismissible" role="alert">
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-                                    Designer has been succesfully added.
+                                    Product has been succesfully added.
                                 </div>
                                <table class="projects_table table table-striped table-bordered table-hover dt-responsive display">
                                     <thead>
-                                        <th>Designer ID</th>
-                                        <th>Designer Name</th>
-                                        <th>Designer Email</th>
-                                        <th>Designer Phone</th>
+                                        <th>Image</th>
+                                        <th>Product ID</th>
+                                        <th>Product Name</th>
+                                        <th>Product Manufacturer</th>
+                                        <th>Temp</th>
+                                        <th>Initial Price</th>
+
                                     </thead>
                                 </table>
                             </div>
@@ -55,13 +50,11 @@
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h4 class="modal-title">Add new designer</span></h4>
+                        <h4 class="modal-title">Add new Product</span></h4>
                     </div>
                     <div class="modal-body">
                         <?php 
-
                             include($_MPATH['ADMIN_VIEWS'].'add_designer_form_view.php');
-
                          ?>
                     </div>
                     <div class="modal-footer">
