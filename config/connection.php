77e5c279ecd0b81ef8959faa5ed95741bc4646e7
eduardbindila <eduard.bindila@gmail.com>
2021-03-14 19:45:54 +0100

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -534,7 +534,7 @@ Class LoadHTMLArtefacts{
 	//Set&Get Links
 	//====================
 	function setLink($href) {
-		array_push($this->links, $href.'?v=0.1.951');
+		array_push($this->links, $href.'?v=0.1.952');
 	}
 
 
@@ -548,7 +548,7 @@ Class LoadHTMLArtefacts{
 	//Set&Get Scripts
 	//====================
 	function setScript($src) {
-		array_push($this->scripts, $src.'?v=0.1.951');
+		array_push($this->scripts, $src.'?v=0.1.952');
 	}
 
 
