db088ef5b65dda841f547636e8d01b54fed99f24
eduardbindila <eduard.bindila@gmail.com>
2021-10-11 01:51:46 +0200

    Update update_legacy_products.php

--- modules/admin/manage/controllers/update_legacy_products.php
+++ modules/admin/manage/controllers/update_legacy_products.php
@@ -31,7 +31,7 @@ $updateListsQuery = $QueryBuilder->select(
 	$options = array(
 		"table" => "products_import",
 		"columns" => "*",
-		"where" => "import_product_list_id=".$getLatestListId[0]['latest_list_id']." and status in  (7, 2)",
+		"where" => "import_product_list_id=".$getLatestListId[0]['latest_list_id']." and status in  (7)",
 		"limit" => "100"
 	)
 );
@@ -122,7 +122,7 @@ foreach ($updateListsQuery as $product => $product_details) {
 		$comment = 'Product not found';
 	}
 
-echo $thisProductQuery.' => '.$legacyProductQuery;
+
 
 	$updateImportProductQuery = $QueryBuilder->update(
 		$conn,
