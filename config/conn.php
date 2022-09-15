ca265e9d891762e0086b90167a10434edefeffa5
eduardbindila <eduard.bindila@gmail.com>
2022-09-15 20:15:40 +0200

    changed version

--- config/conn.php
+++ config/conn.php
@@ -658,7 +658,7 @@ Class LoadHTMLArtefacts{
 	//Set&Get Links
 	//====================
 	function setLink($href) {
-		array_push($this->links, $href.'?v=0.1.97121543');
+		array_push($this->links, $href.'?v=0.1.98');
 	}
 
 
@@ -672,7 +672,7 @@ Class LoadHTMLArtefacts{
 	//Set&Get Scripts
 	//====================
 	function setScript($src) {
-		array_push($this->scripts, $src.'?v=0.1.97121543');
+		array_push($this->scripts, $src.'?v=0.1.98');
 	}
 
 
