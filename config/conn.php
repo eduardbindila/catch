e7b2ad7e01bf40d2445ec328930c32a8f9eb8a06
eduardbindila <eduard.bindila@gmail.com>
2022-09-22 19:14:32 +0200

    updated version

--- config/conn.php
+++ config/conn.php
@@ -658,7 +658,7 @@ Class LoadHTMLArtefacts{
 	//Set&Get Links
 	//====================
 	function setLink($href) {
-		array_push($this->links, $href.'?v=0.1.981');
+		array_push($this->links, $href.'?v=0.1.982');
 	}
 
 
@@ -672,7 +672,7 @@ Class LoadHTMLArtefacts{
 	//Set&Get Scripts
 	//====================
 	function setScript($src) {
-		array_push($this->scripts, $src.'?v=0.1.981');
+		array_push($this->scripts, $src.'?v=0.1.982');
 	}
 
 
