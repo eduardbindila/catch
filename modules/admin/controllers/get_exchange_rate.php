34767b463fb8770b47ffcc46159e355873afc801
eduardbindila <eduard.bindila@gmail.com>
2022-11-10 19:59:05 +0100

    updated exchange rate to get the latest rate if 0

--- modules/admin/controllers/get_exchange_rate.php
+++ modules/admin/controllers/get_exchange_rate.php
@@ -4,41 +4,27 @@ require_once('../../../config/helpers.php');
 require_once($_PATH['COMMON_BACKEND'].'functions.php');
 
 
-	$date = $_POST['date'];
-
-	$dateObj = date_parse_from_format("d/m/Y", $date);
-
-	
-
-	if($dateObj['error_count'] > 0) {
-		$dateObj = date_parse_from_format("Y-m-d", $date);
-	}
-
-	$cubeDate = date("Y-m-d", strtotime($date));
-
-	// returns original date string assuming the format was Y-m-d H:i:s
-	$cubeDate = date('Y-m-d ', mktime($dateObj['hour'], $dateObj['minute'], $dateObj['second'], $dateObj['month'], $dateObj['day'], $dateObj['year'])); 
+function getExchangeRateByDate($year, $date)
+{
 
 
-	$url = "https://www.bnr.ro/files/xml/years/nbrfxrates".$dateObj['year'].".xml";
+	$url = "https://www.bnr.ro/files/xml/years/nbrfxrates".$year.".xml";
 
 	//echo $url;
 
 	$xml = simplexml_load_file($url);
 
-	$exchange_rate = 0;
-
-
 	$currency = [];
+
+
+	$exchange_rate = 0;
 
 	foreach($xml->Body->Cube as $cube) {
 
 		$cubeDateXML_dt = new DateTime($cube['date']);
 
-		$cubeDate_dt = new DateTime($cubeDate);
 
-
-		if($cubeDateXML_dt == $cubeDate_dt) {
+		if($cubeDateXML_dt == $date) {
 
 			//echo $cubeDateXML_dt.' si '.$cubeDate_dt; 
 
@@ -54,9 +40,48 @@ require_once($_PATH['COMMON_BACKEND'].'functions.php');
 	        	
 	    	}
 		}
+		//print_R($currency);
 
 		
 	}
+
+	return $exchange_rate;
+
+}
+
+
+	$date = $_POST['date'];
+
+	$dateObj = date_parse_from_format("d/m/Y", $date);
+
+	
+
+	if($dateObj['error_count'] > 0) {
+		$dateObj = date_parse_from_format("Y-m-d", $date);
+	}
+
+	$cubeDate = date("Y-m-d", strtotime($date));
+
+	// returns original date string assuming the format was Y-m-d H:i:s
+	$cubeDate = date('Y-m-d ', mktime($dateObj['hour'], $dateObj['minute'], $dateObj['second'], $dateObj['month'], $dateObj['day'], $dateObj['year'])); 
+
+	$cubeDate_dt = new DateTime($cubeDate);
+
+	$exchange_rate = 0;
+
+	$exchange_rate = getExchangeRateByDate($dateObj['year'], $cubeDate_dt);
+
+
+	while ($exchange_rate == 0) {
+
+		$exchange_rate = getExchangeRateByDate($dateObj['year'], $cubeDate_dt);
+
+		$cubeDate_dt = $cubeDate_dt->modify('-1 day');
+	}
+
+
+	
+	
 
 	// print_r($currency[0]['value'];
 
