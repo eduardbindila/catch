f431180b591ac01953313fc160b5375d44163ea2
eduardbindila <eduard.bindila@gmail.com>
2023-04-17 13:11:30 +0200

    Update update_sylvania_product_details.php

--- crons/update_sylvania_product_details.php
+++ crons/update_sylvania_product_details.php
@@ -20,7 +20,7 @@ $root = "https://www.sylvania-lighting.com/product/en-int/products/";
             "where" => "manufacturer ='syl' AND last_crawled_date <= CURRENT_DATE() - INTERVAL 1 MONTH",
             "orderBy" => "last_crawled_date",
             "orderType" => "ASC",
-            "limit" => "1"
+            "limit" => "20"
 
         ),
         $returnType = 'insertedProducts'
@@ -67,7 +67,7 @@ CLASS ScrapHelpers {
 foreach ($productQuery as $key => $value) {
     # code...
 
-    $key = '0029106';
+    //$key = '0029106';
 
     $html = file_get_contents($root.$key.'/');
 
